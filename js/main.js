ebfe9f284432d2f5be5446ff1253f3e62d535eef
johnszetho <jszetho@gmail.com>
2022-02-16 23:13:34 +1100

    implement jumping loading dots

--- js/main.js
+++ js/main.js
@@ -6,3 +6,11 @@ console.log(`Height of element id=\"deets\" = ${deetsHeight}px`);
 
 // setProperty to use in CSS
 document.documentElement.style.setProperty('--deetsH', `${deetsHeight}px`);
+
+var animation = lottie.loadAnimation({
+    container: document.getElementById('anim'), // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'LoadingDots.json' // the path to the animation json
+  });
